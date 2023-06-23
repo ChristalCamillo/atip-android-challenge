4b7fbff807ed92abc6f6c95b990699bb850d8ac9
christal <christal.b.camillo@gmail.com>
2023-06-23 17:56:16 -0300

    updating gradle version to 34

--- app/src/main/java/com/olxbr/android/challenge/listing/ListingActivity.kt
+++ app/src/main/java/com/olxbr/android/challenge/listing/ListingActivity.kt
@@ -5,10 +5,11 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.viewModels
 import androidx.compose.runtime.collectAsState
+import com.olxbr.android.challenge.listing.data.datasource.remote.adsService
 
 class MainActivity : ComponentActivity() {
 
-    private val viewModel: ListingViewModel by viewModels { ListingViewModelFactory() }
+    private val viewModel: ListingViewModel by viewModels { ListingViewModelFactory(adsService) }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
